3ec5ae9aae8d6795520d16d2f2e0b1df74b38831
夏芒 <oncwnuJrgYSQpPbWADuchhqKqnR0@git.weixin.qq.com>
2024-12-03 19:33:46 +0800

    Update: 1.1版本，已经完成整套后端的通知体系。

---
+++ README.md
@@ -0,0 +1,206 @@
+# TenX 移动网站 开发文档
+## TODO
+- 前端
+    - /app 页面
+        - 课表页面UI连贯优化（翻页效果）
+        - 课表页面尽量在屏幕内显示完整，避免向下滑动才能完整阅读的情况
+        - 通知页面，换页按钮移到页面顶部，方便换页（或者固定在页面下方，避免拖动才能换页的情况）
+        - 交流板块
+            - 腾创班班委信箱
+            - 团队匿名讨论
+    - 主站页面
+        - 博客、介绍、备案号
+- 后端
+    - 建立数据库，提供以下内容的读写支持
+        - 账户系统
+        - 通知、作业
+        - 课表
+        - 一些重要文件（迷你云盘）
+
+## API
+我们的后端是另外一个独立项目，前端、App等通过API进行访问。
+
+所有API通过 
+
+> tengchuang.top/api
+
+访问。
+
+
+### 获取课表
+> GET tengchuang.top/api/schedule?week=1 
+
+改变其中的 week 值 获得不同周的课表。
+### 解释说明：返回通知的格式
+
+通知组成：
+> uid : 通知条目的唯一编号  
+> title : 标题  
+> type : 类型  
+> sender : 发送者  
+> valid : 有效日期，在此日期前有效
+
+标识ID（UID）的命名为 发送时间YYYYMMDD + 自增序列号
+
+自增序列号的长度不限，日期固定是前八位
+
+当消息被添加时：取历史最新一条消息，若当前消息添加时间与历史最新一条消息时间相同，就让自增序列号自增1而日期不变；若时间不同或没有最新一条消息，就让UID变为当前的日期，自增序列号为1：YYYYMMDD1
+
+（在数据库存储时，每一个数据会获得自己的排序id（自增），与UID不同，用于快速取出前x项。最新保存的项会获得最大的排序id。第一个存入的，排序id为1，第二个存入的为2，第1000个存入的为1000，以此类推）
+
+### 获取通知列表
+
+参数：
+> type : 类型（valid或为all）   
+> page : 页数（取0为页码数。取数字为页数）
+改变其中的 page 值 获得对应页数的通知，**每页最多5条**。
+## type = valid
+### 1  输入页码 page = xx
+> GET tengchuang.top/api/notice?type=valid&page=1
+页数为1，后端会根据排序ID，取最新（排序ID最大）的5条通知；页数为2，取6~10条（如果存在），以此类推
+返回的json格式：  
+### **（注意大小写）**
+~~~json
+[
+  {
+    "uid": 202411201,
+    "title": "通知标题1",
+    "type": "通知类型1",
+    "sender": "发送者1",
+    "content": "通知内容1",
+    "valid": "20241123"
+  },
+  {
+    "uid": 202411202,
+    "title": "通知标题2",
+    "type": "通知类型2",
+    "sender": "发送者2",
+    "content": "通知内容1",
+    "valid": "20241124"
+  }
+]
+~~~
+
+注释：
+> 通知标题不超过30字,  
+> 通知类型不超过10字,  
+> 发送者不超过10字,  
+> 通知内容不超过5000字。
+
+### 2  当 page 值 为 0 时，返回所有通知的**条数**与每页显示的**条数**
+> GET tengchuang.top/api/notice?type=valid&page=0
+（如12项通知，5条/页）
+~~~json
+{
+    "counts": 12,
+    "slice": 5
+}
+~~~
+
+## type = all
+### 1 输入页码 page = xx，返回对应通知，无论是否过期
+> GET tengchuang.top/api/notice?type=all&page=1
+### 2 当 page 值 为 0 时，返回所有通知的**条数**与每页显示的**条数**，无论是否过期。
+> GET tengchuang.top/api/notice?type=all&page=0
+
+# 添加通知
+> POST tengchuang.top/api/notice
+
+POST发送的json格式：  
+（注意大小写）
+~~~json
+{
+  "params": {
+    "operate": "add",
+    "notice_data": {
+      "token": "口令，如果正确才能添加通知",
+      "title": "通知标题",
+      "type": "通知类型（作业等）",
+      "sender": "发送者",
+      "content": "通知内容",
+      "valid": "在此日期前有效（含当日）"
+    }
+  }
+}
+~~~
+在 NoticeData 中 输入 Token、Title、Type、Sender、Content、Valid，令Operate = "add"，并用 params 包起来，通过 JSON 格式发送。
+
+开发环境中token为123456，正式环境待定。
+
+发送成功后，服务器会返回一个 JSON，格式如下：
+
+返回的json格式：
+~~~json
+{
+  "result": "success/failed",
+  "message": "成功/失败原因"
+}
+~~~
+
+### 更改已有通知
+> POST tengchuang.top/api/notice
+
+在前端的更改页面，应该先获取通知的UID、发送者、标题什么的，   
+先在页面中填入原有的通知消息，   
+因为更改是相当于完全更改，假设你只改标题，其他不变，   
+那就用户会看到原来的消息，仅需更改标题，其他不变，发送整个新表单过来，后端会自动根据UID，覆盖发送者、标题、内容等。   
+
+（注意大小写）
+
+POST发送的json格式：
+~~~json
+{
+  "params": {
+    "operate": "update",
+    "update_data": {
+      "token": "口令，如果正确才能添加通知",
+      "title": "通知标题",
+      "type": "通知类型（作业等）",
+      "sender": "发送者",
+      "content": "通知内容",
+      "valid": "在此日期前有效（含当日）"
+    }
+  }
+}
+
+~~~
+在 ModifyData 中 输入 要更改的UID，以及对应的Token、Title、Type、Sender、Content、Valid，令Operate = "modify"，并用 params 包起来，通过 JSON 格式发送。
+
+开发环境中token为123456，正式环境待定。
+
+发送成功后，服务器会返回一个 JSON，格式如下：
+
+返回的json格式：
+~~~json
+{
+  "result": "success/failed",
+  "message": "成功/失败原因"
+}
+~~~
+
+### 删除通知
+> POST tengchuang.top/api/notice
+（注意大小写）
+POST发送的json格式：
+~~~json
+{
+    "params": {
+        "operate": "delete",
+        "delete_data": {
+            "uid": "通知的UID",
+            "token": "口令，如果正确才能删除通知",
+        },
+    }
+}
+ 在 DeleteData 中 输入 要删除的UID，以及对应的Token，令Operate = "delete"，并用 params 包起来，通过 JSON 格式发送。
+~~~
+开发环境中token为123456，正式环境待定。
+
+发送成功后，服务器会返回一个 JSON，格式如下：
+返回的json格式：
+~~~json
+{
+  "result": "success/failed",
+  "message": "成功/失败原因"
+}
+~~~
--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Notice(Resource):
 
     # 声明类型
     type : str # Page/其他功能
-    page : str # Type为Page时，返回页码
+    page : str # type为Page时，返回页码
 
     def get(self):
 
@@ -65,7 +65,7 @@ class Notice(Resource):
             
                 result = {
                     "counts": counts, # 总条数
-                    "itemsPerPage": Global_ShowPages, # 每页显示的条数
+                    "slice": Global_ShowPages, # 每页显示的条数
                 }
                 return result
             else:
@@ -81,7 +81,7 @@ class Notice(Resource):
             current_time = time.strftime("%Y%m%d") # 取现行时间
 
             if page == "0":
-                counts = DatabaseOperate.Select_Database("Notifications",f"WHERE Valid >= {current_time} ",Count = True) # 查询符合条件的通知总数
+                counts = DatabaseOperate.Select_Database("Notifications",f"WHERE valid >= {current_time} ",Count = True) # 查询符合条件的通知总数
             
                 result = {
                     "counts": counts, # 总条数
@@ -96,13 +96,13 @@ class Notice(Resource):
                     return {'result': 'failed','message': 'Page parameter is invalid,provide a integer'}, 400
                 
 
-                return DatabaseOperate.Select_Database("Notifications",f"WHERE Valid >= {current_time} ORDER BY id DESC LIMIT {Global_ShowPages} OFFSET {ThePage};")
+                return DatabaseOperate.Select_Database("Notifications",f"WHERE valid >= {current_time} ORDER BY id DESC LIMIT {Global_ShowPages} OFFSET {ThePage};")
             
         return {'result': 'failed','message': 'Invalid Requests.'}, 400
 
-    NoticeData : str # 要添加的数据
-    ModifyData : str # 要修改的数据
-    DeleteData : str # 要删除的数据
+    notice_data : str # 要添加的数据
+    update_data : str # 要修改的数据
+    delete_data : str # 要删除的数据
 
     token : str # 输入token以获得写通知权限。 
     data : str # 接收到的原始请求json
@@ -113,37 +113,37 @@ class Notice(Resource):
         if "params" in data:
             params = data['params'] # 接收到的参数，里面params涵盖了所有信息
 
-            if "Operate" not in params:
-                return {'result': 'failed','message': '"Operate" Parameter is required'}, 400
+            if "operate" not in params:
+                return {'result': 'failed','message': '"operate" Parameter is required'}, 400
             
-            if params["Operate"] == "add":
+            if params["operate"] == "add":
             
-                if "NoticeData" in params:
+                if "notice_data" in params:
 
-                    return self.AddContent(params["NoticeData"])
+                    return self.AddContent(params["notice_data"])
                 # 添加通知，处理NoticeData
                 
-                return {'result': 'failed','message': '"NoticeData" Parameter is required'}, 400
+                return {'result': 'failed','message': '"notice_data" Parameter is required'}, 400
                 
 
-            if params["Operate"] == "modify":
+            if params["operate"] == "update":
 
-                if "ModifyData" in params:
+                if "update_data" in params:
 
-                    return self.ModifyContent(params["ModifyData"])
-                # 修改通知，处理ModifyData
+                    return self.UpdateContent(params["update_data"])
+                # 修改通知，处理update_data
 
-                return {'result': 'failed','message': '"ModifyData" Parameter is required'}, 400
+                return {'result': 'failed','message': '"update_data" Parameter is required'}, 400
                 
-            if params["Operate"] == "delete":
-                if "DeleteData" in params:
+            if params["operate"] == "delete":
+                if "delete_data" in params:
 
-                    return self.DeleteContent(params["DeleteData"])
+                    return self.DeleteContent(params["delete_data"])
                 # 删除通知，处理DeleteData
 
-                return {'result': 'failed','message': '"DeleteData" Parameter is required'}, 400
+                return {'result': 'failed','message': '"delete_data" Parameter is required'}, 400
                 
-            return {'result': 'failed','message': '"Operate" Parameter is required'}, 400
+            return {'result': 'failed','message': '"operate" Parameter is required'}, 400
 
         return {'result': 'failed','message': '"Params" Parameter is required'}, 400
     
@@ -161,92 +161,90 @@ class Notice(Resource):
         
         try:
 
-            token = NoticeData["Token"]
+            token = NoticeData["token"]
 
-            title = NoticeData["Title"]
-            type = NoticeData["Type"]
-            sender = NoticeData["Sender"]
-            content = NoticeData["Content"]
-            valid = NoticeData["Valid"]
+            title = NoticeData["title"]
+            type = NoticeData["type"]
+            sender = NoticeData["sender"]
+            content = NoticeData["content"]
+            valid = NoticeData["valid"]
         except Exception as e:
-            return {'error': '缺失必要的项 make sure you have Token,Title,Type,Sender,Content,Valid'}, 400
+            return {'error': '缺失必要的项 make sure you have token,title,type,sender,content,valid'}, 400
 
         if token == Global_Temp_Token:
             
             current_time = time.strftime("%Y%m%d")
-            try:
-                DatabaseOperate.hang_on = True # 延缓下一次关闭数据库，防止被关闭无法操作
-                Latest = DatabaseOperate.Select_Database("Notifications","ORDER BY id DESC LIMIT 1;") # 最新的通知
-                
-                if Latest["UID"]: # 如果最新的UID存在的话
-                    UID = Latest["UID"] # 获取最新的UID
-                    if UID[0:8] == current_time: # 如果日期（前八位）相同，就让后面编号自增1
-                        local = int(UID[8:]) # 临时变量，储存末尾的自增序列号
-                        local += 1 # 序列号自增1
-                        UID = f"{current_time}{local}" # 自增完加到末尾
-                    else: # 如果日期（前八位）不同，就让UID为当天的第一个通知
-                        UID = f"{current_time}1"
-                else: # 如果没有任何通知，就让UID为当天的第一个通知
-                    UID = f"{current_time}1"
-            except:
-                UID = f"{current_time}1"
+            DatabaseOperate.hang_on = True # 延缓下一次关闭数据库，防止被关闭无法操作
+            Latest = DatabaseOperate.Select_Database("Notifications","ORDER BY id DESC LIMIT 1;") # 最新的通知
+            print(Latest)
+            if "uid" in Latest[0]: # 如果最新的uid存在的话
+                uid = Latest[0]["uid"] # 获取最新的uid
+                if uid[0:8] == current_time: # 如果日期（前八位）相同，就让后面编号自增1
+                    local = int(uid[8:]) # 临时变量，储存末尾的自增序列号
+                    local += 1 # 序列号自增1
+                    uid = f"{current_time}{local}" # 自增完加到末尾
+                else: # 如果日期（前八位）不同，就让uid为当天的第一个通知
+                    uid = f"{current_time}1"
+            else: # 如果没有任何通知，就让uid为当天的第一个通知
+                uid = f"{current_time}1"
 
             DatabaseOperate.Insert_Database("Notifications",{
-                "Title":title,
-                "Type":type,
-                "Sender":sender,
-                "Content":content,
-                "Valid":valid,
-                "UID":UID,
+                "title":title,
+                "type":type,
+                "sender":sender,
+                "content":content,
+                "valid":valid,
+                "uid":uid,
                 })
 
 
 
             back = {  # 成功处理，返回结果
                 'Date':current_time,
-                'Title':title,
-                'Sender':sender,
+                'title':title,
+                'sender':sender,
             } 
 
             return back
 
         return "密码错误，添加失败"
 
-    def ModifyContent(self,ModifyData):
+    def UpdateContent(self,update_data):
         try:
-            token = ModifyData["Token"]
-            title = ModifyData["Title"]
-            type = ModifyData["Type"]
-            sender = ModifyData["Sender"]
-            content = ModifyData["Content"]
-            valid = ModifyData["Valid"]
-            UID = ModifyData["UID"]
+            token = update_data["token"]
+            title = update_data["title"]
+            type = update_data["type"]
+            sender = update_data["sender"]
+            content = update_data["content"]
+            valid = update_data["valid"]
+            uid = update_data["uid"]
 
             send = {
-                "Title":title,
-                "Type":type,
-                "Sender":sender,
-                "Content":content,
-                "Valid":valid,
+                "title":title,
+                "type":type,
+                "sender":sender,
+                "content":content,
+                "valid":valid,
             }
 
             if token == Global_Temp_Token:
-                DatabaseOperate.Update_Database("Notifications",f"UID = {UID}", send)
+                result = DatabaseOperate.Update_Database("Notifications",f"uid = {uid}", send)
+                return result
         except:
-            return {'result': 'failed','message': '缺失必要的项 make sure you have UID,Token,Title,Type,Sender,Content,Valid'}, 400
+            return {'result': 'failed','message': '缺失必要的项 make sure you have uid,token,title,type,sender,content,valid'}, 400
         
         
     def DeleteContent(self,DeleteData):
         try:
-            token = DeleteData["Token"]
-            UID = DeleteData["UID"]
+            token = DeleteData["token"]
+            uid = DeleteData["uid"]
 
             if token == Global_Temp_Token:
-                DatabaseOperate.Delete_Database("Notifications",f"UID = {UID}")
+                DatabaseOperate.Delete_Database("Notifications",f"uid = {uid}")
         except:
-            return {'error': '缺失必要的项 make sure you have UID,Token'}, 400
+            return {'result': 'failed','message': '缺失必要的项 make sure you have uid'}, 400
         
-        return "删除成功"
+        return {'result': 'success','message': f'已删除{uid}（若存在）'}
         
     
         
@@ -304,11 +302,11 @@ class DatabaseOperate():
                 #遍历结果
                 for row in results :
                     notices.append({
-                        "UID": row[6],
-                        "Title": row[1],
-                        "Type": row[2],
-                        "Sender": row[3],
-                        "Content": row[4],
+                        "uid": row[6],
+                        "title": row[1],
+                        "type": row[2],
+                        "sender": row[3],
+                        "content": row[4],
                         "valid": str(row[5]),
                     })
             except Exception as e:
@@ -321,18 +319,18 @@ class DatabaseOperate():
             return notices
     
     @staticmethod
-    def Insert_Database(TableName:str,Content:dict) -> bool:
+    def Insert_Database(TableName:str,content:dict) -> bool:
         """
         连接到数据库，并进行插入操作
         插入格式为
         {
-            "Title": " ",
-            "Type": " ",
-            "Sender": " ",
-            "Content": " ",
-            "Valid": " ",
+            "title": " ",
+            "sender": " ",
+            "type": " ",
+            "content": " ",
+            "valid": " ",
         }
-        UID会在此程序中自己生成。
+        uid会在此程序中自己生成。
         returns:
             bool：插入成功返回True，插入失败抛出异常
 
@@ -346,16 +344,16 @@ class DatabaseOperate():
             # 2.插入操作
             # 编写sql 查询语句 对应 表名Notifications
             # 使用参数化查询，防止注入
-            sql = f"INSERT INTO {TableName} (title, type, sender, content, valid, UID) VALUES (%s, %s, %s, %s, %s, %s)"
+            sql = f"INSERT INTO {TableName} (title, type, sender, content, valid, uid) VALUES (%s, %s, %s, %s, %s, %s)"
 
-            values = (Content["Title"], Content["Type"], Content["Sender"], Content["Content"], Content["Valid"], Content["UID"])
+            values = (content["title"], content["type"], content["sender"], content["content"], content["valid"], content["uid"])
 
             cur.execute(sql, values)  # 执行sql语句
             DatabaseOperate.db.commit()
             
             turnback = {
                 "result": "success",
-                "message": f"插入数据成功，UID{Content['UID']},Title{Content['Title']}"
+                "message": f"插入数据成功，uid{content['uid']},title{content['title']}"
             } # 返回结果
 
             return turnback
@@ -371,7 +369,7 @@ class DatabaseOperate():
 
 
     @staticmethod
-    def Update_Database(TableName:str,Condition:str,UpdateDict:dict) -> bool:
+    def Update_Database(TableName:str,Condition:str,UpdateDict:dict):
         """
         连接到数据库，并进行更新操作
         params:
@@ -380,7 +378,7 @@ class DatabaseOperate():
             UpdateDict: Dict 更新内容
 
         examples：
-            Update_Database("Notifications","id = 1",{"Title":"Test","Type":"test","Sender":"test","Content":"test","Valid":"test"})
+            Update_Database("Notifications","id = 1",{"title":"Test","type":"test","sender":"test","content":"test","valid":"test"})
 
         returns:
             bool：更新成功返回True，更新失败抛出异常
