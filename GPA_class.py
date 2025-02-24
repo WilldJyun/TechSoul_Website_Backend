1df5d8733f705cd13335fcc3fba4fb12fcef800b
Jyun Willd <892640097@qq.com>
2025-02-24 22:49:05 +0800

    Fix: 小修复，增加了输入格式错误时的返回提示，修复rank不显示的问题。

--- GPA_class.py
+++ GPA_class.py
@@ -9,36 +9,41 @@ class GPA_class(Resource):
 
             data : dict = request.get_json()
             if not data:
-                return {'result':'error','message':'No data'},400
+                return {'result':'failed','message':'No data'},400
             
             if 'operate' not in data:
-                return {'result':'error','message':'operate not in data'},400
+                return {'result':'failed','message':'operate not in data'},400
             
             if data['operate'] == 'GPA':
                 if 'params' not in data:
-                    return {'result':'error','message':'params not in data'},400
+                    return {'result':'failed','message':'params not in data'},400
                 params = data['params']
                 print(params)
 
                 if 'studentID' not in params: # 注意大小写，下同
-                    return {'result':'error','message':'studentID not in data'},400
+                    return {'result':'failed','message':'studentID not in data'},400
                 
                 if 'password' not in params:
-                    return {'result':'error','message':'password not in data'},400
+                    return {'result':'failed','message':'password not in data'},400
                 
                 GPA_DatabaseOperate.initialise()
                 result = GPA_DatabaseOperate.CheckPassword(params['studentID'],params['password'])
                 if result == False:
-                    return {'result':'error','message':'Wrong password or studentID'},400 # 查询有误，返回这个
-                
+                    return {'result':'failed','message':'Wrong password or studentID'},400 # 查询有误，返回这个
+                    
+                try:
                 # 查询成功
-                now = datetime.now()
-                turnback = {
-                    'name':result['name'],
-                    'studentID':params['studentID'],
-                    'GPA':result['GPA'],
-                    'rank':f"result['rank'] / 31 人（班内排名）", # 后续需要在这里修改查询结果
-                    'time':now.strftime('%Y-%m-%d %H:%M:%S')
-                } # 返回结果
-                return {'result':'success','message':turnback},200
+                    now = datetime.now()
+                    turnback = {
+                        'name':result['name'],
+                        'studentID':params['studentID'],
+                        'GPA':result['GPA'],
+                        'rank':f"{result['rank']} / 31 人（班内排名）", # 后续需要在这里修改查询结果
+                        'time':now.strftime('%Y-%m-%d %H:%M:%S')
+                    } # 返回结果
+                    return {'result':'success','message':turnback},200
+                
+                except Exception as e:
+                    print(e)
+                    return {'result':'failed','message':'Invalid input. 输入格式可能不正确'},500
             
