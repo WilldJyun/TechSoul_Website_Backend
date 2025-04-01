6910f7bfddfcb3876fa4f2610857f85999e1b6f8
Jyun Willd <892640097@qq.com>
2025-04-01 16:08:58 +0800

    refactor(Alzheimer_class): remove unused example content
    - Deleted the entire example content block for a cleaner code structure

--- Alzheimer_class.py
+++ Alzheimer_class.py
@@ -5,16 +5,6 @@ from Global_Vars import *
 from alzheimer_infer.infer_with_possibility import predict
 from alzheimer_infer.risk_prompt import  get_not_predict_yet_prompt, get_low_risk_prompt, get_medium_high_risk_prompt, get_medium_low_risk_prompt, get_medium_risk_prompt
 
-content = """她 62 岁了，女性，没怎么上过学。
-身高 150 厘米，体重 55 公斤。她从不抽烟喝酒。
-家族里没有人得阿尔茨海默病，也没有心血管疾病和糖尿病。
-她有过一次头部受伤，是几年前不小心摔倒撞到了头。
-他没有抑郁症，但是有高血压。我每天会去楼下溜达溜达，身体活动量一般般，饮食就是家常便饭，睡眠质量不太好，容易醒。
-我的收缩压 150 毫米汞柱，舒张压 90 毫米汞柱，总胆固醇 5.5，低密度脂蛋白胆固醇 3.0，高密度脂蛋白胆固醇 1.0，甘油三酯 1.5。
-他有时候会忘记带钥匙，但是生活任务都能完成，也没有行为问题和人格改变，空间方向也没问题，就是有时候会有点糊涂。
-简易精神状态检查表评分 22 分，日常生活活动能力表评分 90 分，
-功能独立性评测我觉得自己算 4 分，需要一点帮助。"""
-
 class Alzheimer_class(Resource):
 
     def get(self):
